a85cb749c50bd58854ccff18f843653eafdad933
Barys Marukhin <barys_marukhin@epam.com>
2023-09-28 00:00:33 +0400

    [Red] knows that "hey" is not between 5 and 15 characters long

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -1,9 +1,36 @@
+/*
+* Between 5 and 15 characters long.
+* Contains at least one digit.
+* Contains at least one upper case letter.
+*
+* Return an object containing a boolean result
+* and an errors key that
+* — when provided with an invalid password
+* — contains an error message or type for all errors in occurrence.
+* There can be multiple errors at a single time.
+*
+* */
+
+/*
+*
+* Incoming string: 'JohnDoe15'
+* {
+*   result: true,
+*   errors: ['InvalidLength', 'NoDigitsContains', 'NoUppercaseLettersContains']
+* }
+*
+* */
+
+import { PasswordValidator } from "./index";
 
 describe('password validator', () => {
+  it('knows that "hey" is not between 5 and 15 characters long', () => {
+    const { result, errors } = PasswordValidator.validate('hey');
 
-  test('hello', () => {
-    expect("between 5 and 15").toContain('5 and 15')
-  })
+    expect(result).toBe(false);
+    expect(errors).toHaveLength(1);
+    expect(errors).toContain('InvalidLength');
+  });
 })
 
 
