eca8bd827ce0e7b177e058df5711054467f3ea2e
Barys Marukhin <barys_marukhin@epam.com>
2023-09-28 00:20:23 +0400

    [Green] knows that "hello" is between 5 and 15 characters long

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -1,9 +1,17 @@
 type ErrorCode = 'InvalidLength' | 'NoDigitsContains' | 'NoUppercaseLettersContains';
 export class PasswordValidator {
     public static validate(input: string): { result: boolean; errors: ErrorCode[]} {
+        const errors: ErrorCode[] = [];
+        let result = true;
+        const isLengthValid = input.length >= 5 && input.length <= 15;
+
+        if(!isLengthValid) {
+            result = false;
+            errors.push('InvalidLength')
+        }
         return {
-            result: false,
-            errors: ['InvalidLength'],
+            result,
+            errors,
         }
     }
 }
