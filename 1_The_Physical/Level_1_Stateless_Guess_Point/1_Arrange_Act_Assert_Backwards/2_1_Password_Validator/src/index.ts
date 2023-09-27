1d3de310f19c2b853fef81b569259d5ff0b9e5d9
Barys Marukhin <barys_marukhin@epam.com>
2023-09-28 00:39:47 +0400

    [Refactor] knows that input string is between 5 and 15 characters long

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -1,16 +1,15 @@
-type ErrorCode = 'InvalidLength' | 'NoDigitsContains' | 'NoUppercaseLettersContains';
+export type ErrorCode = 'InvalidLength' | 'NoDigitsContains' | 'NoUppercaseLettersContains';
+
 export class PasswordValidator {
     public static validate(input: string): { result: boolean; errors: ErrorCode[]} {
         const errors: ErrorCode[] = [];
-        let result = true;
         const isLengthValid = input.length >= 5 && input.length <= 15;
 
         if(!isLengthValid) {
-            result = false;
             errors.push('InvalidLength')
         }
         return {
-            result,
+            result: errors.length === 0,
             errors,
         }
     }
