5b5f8055645ab2c373e0fe99600858d759631512
Barys Marukhin <barys_marukhin@epam.com>
2023-09-29 17:52:31 +0400

    [Green] knows that "Barys" doesn't contain any digits

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -4,10 +4,11 @@ export class PasswordValidator {
     public static validate(input: string): { result: boolean; errors: ErrorCode[]} {
         const errors: ErrorCode[] = [];
         const isLengthValid = input.length >= 5 && input.length <= 15;
+        const containsDigit = input.split('').some(character => !isNaN(Number(character)))
 
-        if(!isLengthValid) {
-            errors.push('InvalidLength')
-        }
+        if(!isLengthValid) errors.push('InvalidLength')
+        if(!containsDigit) errors.push('NoDigitsContains')
+
         return {
             result: errors.length === 0,
             errors,
