69c6ca4729ae7e3d16a39cbe85f582560a94caea
Barys Marukhin <barys_marukhin@epam.com>
2023-10-05 17:49:13 +0400

    [Red] Execute valid ranges

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,5 +1,18 @@
+import { isTimeRangeValid } from './index';
 
 describe('military time validator', () => {
-
-
+    it.each([
+        ['00:00 - 23:59', true],
+        ['01:30 - 14:32', true],
+        ['12:00 - 13:30', true],
+        ['22:15 - 22:45', true],
+        ['03:00 - 14:10', true],
+        // ['00:00 - 24:00', false], // Invalid end time
+        // ['25:22 - 12:00', false], // Invalid start time
+        // ['01:30', false], // Missing end time
+        // ['12:00-12:34-12:59', false], // Too many times
+        // ['00:05-23:59', false], // Missing spaces around the hyphen
+    ])(`isTimeRangeValid: %s should be %p`, (timeRange, expected) => {
+        expect(isTimeRangeValid(timeRange)).toBe(expected);
+    });
 })
