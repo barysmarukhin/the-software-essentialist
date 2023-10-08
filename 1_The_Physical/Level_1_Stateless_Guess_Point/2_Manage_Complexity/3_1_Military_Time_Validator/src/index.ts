0567921999c03bf43b9e255f0d85d69958564667
Barys Marukhin <barys_marukhin@epam.com>
2023-10-08 14:02:15 +0400

    [Green] Manage if amount of incoming params is insufficient or redundant

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -10,6 +10,9 @@ function isTimeValid(time: string) {
 export const isTimeRangeValid = (timeRange: string) => {
     const timeList = timeRange.split('-').map(time => time.trim());
 
+    // Check if the timeList contains exactly "start - end" items
+    if (timeList.length !== 2) return false;
+
     // Validate the individual times
     const [start, end] = timeList;
     if (!isTimeValid(start) || !isTimeValid(end)) return false;
