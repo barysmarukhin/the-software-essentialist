44a6de82e280f7ad7ade2509e03cd2aee5668c83
Barys Marukhin <barys_marukhin@epam.com>
2023-10-08 13:53:20 +0400

    [Green] Manage invalid start and end times

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,1 +1,18 @@
-export const isTimeRangeValid = (timeRange: string) => true
+function isTimeValid(time: string) {
+    const [hours, minutes] = time.split(':').map(Number);
+
+    // Check if hours and minutes are valid
+    if (isNaN(hours) || isNaN(minutes)) return false;
+    if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59) return false;
+
+    return true;
+}
+export const isTimeRangeValid = (timeRange: string) => {
+    const timeList = timeRange.split('-').map(time => time.trim());
+
+    // Validate the individual times
+    const [start, end] = timeList;
+    if (!isTimeValid(start) || !isTimeValid(end)) return false;
+
+    return true;
+}
