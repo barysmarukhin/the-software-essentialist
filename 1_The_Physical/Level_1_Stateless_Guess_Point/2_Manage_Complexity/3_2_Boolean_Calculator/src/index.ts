e73535d9a86deb6e36b888d9a32800d75c5a698c
Barys Marukhin <barys_marukhin@epam.com>
2023-10-21 15:11:21 +0400

    [Green] evaluates AND operators

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -1,10 +1,15 @@
 export class BooleanCalculator {
     static evaluateExpression(expression: string): boolean {
+
         const resultMap: Record<string, boolean> = {
             TRUE: true,
             FALSE: false,
             'NOT FALSE': true,
             'NOT TRUE': false,
+            'FALSE AND TRUE': false,
+            'TRUE AND FALSE': false,
+            'TRUE AND TRUE': true,
+            'FALSE AND FALSE': false,
         }
 
         return resultMap[expression];
