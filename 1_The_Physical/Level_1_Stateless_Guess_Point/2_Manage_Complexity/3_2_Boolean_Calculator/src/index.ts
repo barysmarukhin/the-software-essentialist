187771381b155f998ad72fff4bc6c1eb3c115964
Barys Marukhin <barys_marukhin@epam.com>
2023-10-28 22:22:00 +0400

    [Refactor] evaluates AND operators

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -1,17 +1,19 @@
 export class BooleanCalculator {
     static evaluateExpression(expression: string): boolean {
 
-        const resultMap: Record<string, boolean> = {
-            TRUE: true,
-            FALSE: false,
-            'NOT FALSE': true,
-            'NOT TRUE': false,
-            'FALSE AND TRUE': false,
-            'TRUE AND FALSE': false,
-            'TRUE AND TRUE': true,
-            'FALSE AND FALSE': false,
+        const resultMap: Record<string, string | boolean> = {
+            TRUE: 'true',
+            FALSE: 'false',
+            NOT: '!',
+            AND: '&&',
         }
 
-        return resultMap[expression];
+        const expressionTokens = expression.split(' ');
+        const javascriptBooleanExpressionAsString = expressionTokens.reduce((acc, part) => {
+            acc += resultMap[part];
+            return acc;
+        }, '')
+
+        return eval(javascriptBooleanExpressionAsString);
     }
 }
